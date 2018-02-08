1cd43099f8be1f79b7117a0994e26d7a99870ca9
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-02-08 19:53:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- item = {itemName: itemPrice}
+ item = new Object();
+ item = {itemName: itemPrice};
 }
 
 function viewCart() {
